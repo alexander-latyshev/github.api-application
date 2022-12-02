4bcd6a0af5bfbe83592a47c31d6af38137ee047a
Alexander Latyshev <aladdelinxr@gmail.com>
2022-12-02 19:43:34 +0300

    Init CRA with ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;

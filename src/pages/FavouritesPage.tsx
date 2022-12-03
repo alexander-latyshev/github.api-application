72305eb0a37f939f5ec04b62093eb6aa70c25399
Alexander Latyshev <aladdelinxr@gmail.com>
2022-12-03 23:46:18 +0300

    Add FavouritesPage

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useAppSelector } from "../hooks/redux";
 
 type Props = {};
 
 const FavouritesPage = (props: Props) => {
+  const favs = useAppSelector((state) => state.github.favourites);
+
+  if (!favs.length) return <p>No items.</p>;
+
   return (
-    <>
-      <h1 className="font-bold">I'M FAVOURITE PAGE!</h1>
-    </>
+    <div className="flex justify-center pt-10">
+      <ul className="list-none">
+        {favs.map((f) => (
+          <li key={f} className="mb-2">
+            <a href={f}>{f}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 

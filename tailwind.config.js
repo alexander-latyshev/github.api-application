1d467a9d82228bfa45613468ccb82462a239193c
Alexander Latyshev <aladdelinxr@gmail.com>
2022-12-03 21:35:40 +0300

    Update config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "github-grey": "#161b22",
+        "github-dg": "#0d1117",
+        "github-col": "#161b22",
+      },
+    },
+  },
+  plugins: [],
+};
